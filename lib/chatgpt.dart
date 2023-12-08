4a8624fe3113392ba42a840bb1051f6f879cad39
FranAliss <franalissar@gmail.com>
2023-12-08 15:17:01 -0400

    Added main menu screen and another screen with GPT 3.5

--- lib/chatgpt.dart
+++ lib/chatgpt.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,15 +8,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ChatGPT Console',
+      title: 'Mobile Examen',
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: ChatScreen(),
+      home: MainMenu(),
     );
   }
 }
 
+// final String apiKey = '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';
+// final String apiKey = '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';
 class ChatScreen extends StatefulWidget {
   @override
   _ChatScreenState createState() => _ChatScreenState();
@@ -26,7 +27,8 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = TextEditingController();
   final List<String> _chatMessages = [];
-  final String apiKey = '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'; 
+
+  final String apiKey = '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';
   void _addMessage(String message) {
     setState(() {
       _chatMessages.add(message);
@@ -46,7 +48,6 @@ class _ChatScreenState extends State<ChatScreen> {
     };
 
     try {
-      // Realizar la solicitud POST a la API de ChatGPT
       http.Response response = await http.post(
         Uri.parse(apiUrl),
         headers: headers,
@@ -55,11 +56,13 @@ class _ChatScreenState extends State<ChatScreen> {
 
       if (response.statusCode == 200) {
         Map<String, dynamic> data = json.decode(response.body);
-        String answer = data['choices'][0]['text'].toString().trim();
+        String answer = utf8.decode(
+            data['choices'][0]['text'].toString().trim().runes.toList());
         _addMessage('Pregunta: $text');
         _addMessage('Respuesta: $answer');
       } else {
-        _addMessage('Error en la solicitud. Código de estado: ${response.statusCode}');
+        _addMessage(
+            'Error en la solicitud. Código de estado: ${response.statusCode}');
       }
     } catch (e) {
       _addMessage('Error al realizar la solicitud: $e');
@@ -69,7 +72,7 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('ChatGPT Console')),
+      appBar: AppBar(title: const Text('Phrase Finisher Console')),
       body: Column(
         children: [
           Expanded(
@@ -81,7 +84,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   return ListTile(
                     title: Text(
                       _chatMessages[index],
-                      style: TextStyle(color: Colors.white),
+                      style: const TextStyle(color: Colors.white),
                     ),
                   );
                 },
@@ -89,21 +92,161 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ),
           Container(
-            margin: EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
             child: Row(
               children: [
                 Expanded(
                   child: TextField(
                     controller: _textController,
-                    decoration: InputDecoration(
-                      hintText: 'Introduce tu pregunta',
+                    decoration: const InputDecoration(
+                      hintText: 'Introduce el prompt',
                       hintStyle: TextStyle(color: Colors.white),
                     ),
-                    style: TextStyle(color: Colors.white),
+                    style: const TextStyle(color: Colors.white),
                   ),
                 ),
                 IconButton(
-                  icon: Icon(Icons.send, color: Colors.white),
+                  icon: const Icon(Icons.send, color: Colors.white),
+                  onPressed: () {
+                    String text = _textController.text.trim();
+                    if (text.isNotEmpty) {
+                      _sendMessage(text);
+                      _textController.clear();
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MainMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Menu'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ChatScreen()),
+                );
+              },
+              child: const Text('Phrase finisher'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NewScreen()),
+                );
+              },
+              child: const Text('GPT 3.5'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewScreen extends StatefulWidget {
+  @override
+  _NewScreenState createState() => _NewScreenState();
+}
+
+class _NewScreenState extends State<NewScreen> {
+  final TextEditingController _textController = TextEditingController();
+  final List<String> _newScreenMessages = [];
+  final String apiKey = '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';
+  void _addMessage(String message) {
+    setState(() {
+      _newScreenMessages.add(message);
+    });
+  }
+
+  void _sendMessage(String text) async {
+    String apiUrl = 'https://api.openai.com/v1/completions';
+    Map<String, String> headers = {
+      'Authorization': 'Bearer $apiKey',
+      'Content-Type': 'application/json',
+    };
+    Map<String, dynamic> body = {
+      'model': 'gpt-3.5-turbo-instruct',
+      'prompt': text,
+    };
+
+    try {
+      http.Response response = await http.post(
+        Uri.parse(apiUrl),
+        headers: headers,
+        body: json.encode(body),
+      );
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = json.decode(response.body);
+        String answer = utf8.decode(
+            data['choices'][0]['text'].toString().trim().runes.toList());
+        _addMessage('Pregunta: $text');
+        _addMessage('Respuesta: $answer');
+      } else {
+        _addMessage(
+            'Error en la solicitud. Código de estado: ${response.statusCode}');
+      }
+    } catch (e) {
+      _addMessage('Error al realizar la solicitud: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Chat GPT 3.5')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.black,
+              child: ListView.builder(
+                itemCount: _newScreenMessages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(
+                      _newScreenMessages[index],
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textController,
+                    decoration: const InputDecoration(
+                      hintText: 'Introduce el prompt',
+                      hintStyle: TextStyle(color: Colors.white),
+                    ),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.send, color: Colors.white),
                   onPressed: () {
                     String text = _textController.text.trim();
                     if (text.isNotEmpty) {
